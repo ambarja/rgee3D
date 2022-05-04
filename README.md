f6fdd2bfaab7098cf16e513731a267627dc52160
ambarja <antony.barja8@gmail.com>
2022-05-04 06:11:29 -0500

    cesar y lesly 💜

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# rgee3D
+# **rgee3D**
+
+A reproducible script to prepare 3D visualization with the integration of **rgee + rayshader** and **R spatial ecosystem**.
+
+<img src="img/rgee3d.png" width='100%'>
+
+<p align="center">
+<a href="https://ko-fi.com/ambarja"><img src="https://img.shields.io/badge/Buy_me_a_coffee-5d83f5?style=for-the-badge&logo=ko-fi&logoColor=white&__cf_chl_managed_tk__=pmd_sOkOcrsQ4T6MRVPX2TeB_mbk2ZryAIn3rNigWIBBC6U-1633993526-0-gqNtZGzNAyWjcnBszQkl" width="150px"></a>
+</p>
---
+++ rgee_rayshader.R
@@ -0,0 +1,77 @@
+#' @autor Antony Barja :3 
+# Requeriments
+library(rgee)
+library(rayshader)
+library(raster)
+library(sf)
+library(magick)
+ee_Initialize()
+
+# Preprocesing population data in rgee
+pop <- ee$ImageCollection$Dataset$WorldPop_GP_100m_pop
+pop_max <- pop$max()
+
+# sf -> featurecollection 
+peru <- st_read("gpkg/peru.gpkg")
+
+# Define new extent for country
+extent <- peru %>% 
+  st_bbox() %>% 
+  st_as_sfc() %>% 
+  sf_as_ee()
+
+# Simple visualization of Peru box
+# Map$addLayer(extent)
+
+# Image to raster o stars ~ 185 s
+get_pop_data <- ee_as_raster(
+  image = pop_max,
+  region = extent,
+  dsn = "/home/ambarja/Documentos/github/rgee3D/population.tif",
+  scale = 1000
+)
+
+# Working in local with raster
+pop_local <- raster("population.tif") %>%
+  crop(peru) %>%
+  mask(peru)
+
+# Preparing raster for rayshader 
+pop_local <- raster_to_matrix(pop_local)
+
+# Visualization 3D
+pop_local %>%
+  sphere_shade(
+    texture = create_texture(
+      "#5e0010", "#5e0010","#5e0010",
+      "#5e0010", "#ffffff")
+    ) %>%
+  plot_3d(pop_local ,
+          zscale = 3,
+          fov = 0, theta = 0,
+          zoom = 0.85,
+          phi = 45,
+          soliddepth = -20,
+          solidcolor = "#5e0010", shadow = TRUE, shadowdepth = -15,
+          shadowcolor = "#5e0010", background = "white",
+          windowsize = c(1200, 1000)
+          )
+
+# render_snapshot(clear=TRUE)
+render_snapshot(
+  "3D_population.png"
+  )
+
+# Magick :3 (Customization)
+edited <- image_read("3D_population.png")
+edited %>%
+  image_annotate(
+    "Population density 2020\n [ rgee + rayshader + R spatial ecosystem ]",
+    gravity = "NorthWest",font = "Bodoni MT",
+    size = 80, degrees = 0, location = "+20+10"
+    ) %>% 
+  image_annotate(
+    "Source: WorlPop | Created by: Antony Barja | @antony_barja",
+    font = "ScriptS", gravity = "SouthWest",
+    size = 15, degrees = 0, location = "+560+10"
+    ) 
